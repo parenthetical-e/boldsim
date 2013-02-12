e9b0e398f55a1c0fbec271441d93da7d019da0f3
Erik Peterson <Erik.Exists@gmail.com>
2013-02-12 14:34:48 -0600

    Redoing many of the run files following the large number of changes to simfMRI

---
+++ bin/run_rwfit_500_learn.py
@@ -0,0 +1,58 @@
+""" A top-level experimental script that run 100 iterations (on 2 cores) of 
+the RW example (see simfMRI.exp_examples.RW()). """
+import functools
+from simfMRI.analysis.plot import hist_t_all_models
+from simfMRI.runclass import Run
+from boldsim.expclass import RWfit
+
+
+class RunRWFit(Run):
+    """ An example of a 100 iteration RW experimental Run(). """
+    
+    def __init__(self):
+        try: 
+            Run.__init__(self)
+        except AttributeError: 
+            pass
+        
+        # ----
+        # An instance of simfMRI.examples.* Class (or similar) 
+        # should go here.
+        self.BaseClass = functools.partial(RWfit, behave="learn")  
+            ## Nornalize the signature of BaseClass with 
+            ## functools.partial
+            ## Expects:
+            ## BaseClass(self.ntrial, TR=self.TR, ISI=self.ISI, prng=prng)
+        
+        # ----
+        # User Globals
+        self.nrun = 500
+        self.TR = 2
+        self.ISI = 2
+        self.model_conf = "rw.ini"
+        self.savedir = "data"
+        self.ntrial = 60
+        
+        # --
+        # Optional Globals
+        self.ncore = 2
+    
+        # ----
+        # Misc
+        self.prngs = None   ## A list of RandomState() instances
+                            ## setup by the go() attr
+
+
+if __name__ == "__main__":
+    sim = RunRWFit()
+    sim.go(parallel=True)
+        ## Results get stored internally.
+
+    # Writing the results to a hdf5    
+    results_name = "rw{0}".format(sim.nrun)
+    sim.save_results(results_name)
+
+    # And plot all the models 
+    # (each is autosaved).
+    hist_t_all_models(sim.savedir, results_name+".hdf5", results_name)
+    
--- expclass.py
+++ expclass.py
@@ -1,10 +1,11 @@
-""" Base classes for model-based fMRI simulations. """
+""" Model-based Exp classes used 'A precise problem with model-based fMRI.' """
 import numpy as np
 import rl
 import simfMRI
 import simBehave
-from simfMRI.template import Exp
+from simfMRI.expclass import Exp
 from simBehave.trials import event_random
+from simfMRI.misc import process_prng
 
 
 class RWfit(Exp):
@@ -12,29 +13,26 @@ class RWfit(Exp):
     Generate and fit behavioral data with a Rescorla-Wagner RL model.
     Use the RPE and values from these fits to run fMRI simulations with
     parameteric regressors.
-    
-    Input:
-    ----
-    n: number of trials
-    behave: kind of behavior to simulate (random or learn).
-    TR: the sampling rate
-    ISI: inter-stimulus interval
     """
-    
-    def __init__(self, n, behave='learn', TR=2, ISI=2):
-        Exp.__init__(self, TR=2, ISI=2)
-
+    def __init__(self, n, behave='learn', TR=2, ISI=2, prng=None):
+        try: 
+            Exp.__init__(self, TR=2, ISI=2, prng=None)
+        except AttributeError: 
+            pass
+        
+        self.prng = process_prng(prng)
+        
         n_cond = 1
         n_trials_cond = n
         trials = []
         acc = []
         p = []
-        if behave is 'learn':
-    		trials, acc, p = simBehave.behave.learn(
-    				n_cond, n_trials_cond, 3, True)
-        elif behave is 'random':
-    			trials,acc,p = simBehave.behave.random(
-                        n_cond,n_trials_cond,3,True)
+        if behave == 'learn':
+    		trials, acc, p, self.prng = simBehave.behave.learn(
+    				n_cond, n_trials_cond, 3, True, self.prng)
+        elif behave == 'random':
+    			trials, acc, p, self.prng = simBehave.behave.random(
+                        n_cond,n_trials_cond, 3, self.prng)
         else:
             raise ValueError(
                     '{0} is not known.  Try learn or random.'.format(behave))
@@ -55,38 +53,34 @@ class RWfit(Exp):
         self.data['best_rl_pars'] = best_rl_pars
         self.data['value'] = values
         self.data['rpe'] = rpes
-        self.data['rand'] = np.random.random(len(self.trials))
+        self.data['rand'] = self.prng.rand(len(self.trials))
 
 
 class RWset(Exp):
-    """ Generate and fit behavioral data with a Rescorla-Wagner RL model,
-    using a given learning rate. 
-    
-    Input:
-    ----
-    n: number of trials
-    behave: kind of behavior to simulate (random or learn).
-    TR: the sampling rate
-    ISI: inter-stimulus interval
-    alpha: the learning rate
-    
-    Use the RPE and values from these fits to run fMRI simulations with
-    parametric regressors. """
-    
-    def __init__(self, n, behave='learn', TR=2, ISI=2, alpha=0.5):
-        Exp.__init__(self, TR=2, ISI=2, alpha=0.5)
-
+    """
+    Generate and behavioral data then use <alpha> to create a Rescorla-Wagner
+    model.  Use the RPE and values from these fits to run fMRI simulations 
+    with parameteric regressors.
+    """
+    def __init__(self, n, alpha=0.5, behave='learn', TR=2, ISI=2, prng=None):
+        try: 
+            Exp.__init__(self, TR=2, ISI=2, prng=None)
+        except AttributeError: 
+            pass
+        
+        self.prng = process_prng(prng)
+        
         n_cond = 1
         n_trials_cond = n
         trials = []
         acc = []
         p = []
-        if behave is 'learn':
-    		trials, acc, p = simBehave.behave.learn(
-                        n_cond, n_trials_cond, 3, True)
-        elif behave is 'random':
-    			trials, acc, p = simBehave.behave.random(
-                        n_cond,n_trials_cond, 3, True)
+        if behave == 'learn':
+    		trials, acc, p, self.prng = simBehave.behave.learn(
+    				n_cond, n_trials_cond, 3, True, self.prng)
+        elif behave == 'random':
+    			trials, acc, p, self.prng = simBehave.behave.random(
+                        n_cond,n_trials_cond, 3, self.prng)
         else:
             raise ValueError(
                     '{0} is not known.  Try learn or random.'.format(behave))
@@ -99,11 +93,12 @@ class RWset(Exp):
         
         # Store the results in appropriate places
         self.trials = np.array(trials)
-        self.durations = np.array([1, ] * len(self.trials)) ## Durations are 1
+        self.durations = np.array([1, ] * len(self.trials))
         self.data['acc'] = acc
         self.data['p'] = p
+        self.data['best_logL'] = best_logL
         self.data['best_rl_pars'] = (alpha, )
         self.data['value'] = values
         self.data['rpe'] = rpes
-        self.data['rand'] = np.random.random(len(self.trials))
+        self.data['rand'] = self.prng.rand(len(self.trials))
 
