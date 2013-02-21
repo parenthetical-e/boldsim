16a9d33af92778b995ea7089559c326cdc8391bb
Erik Peterson <erik.exists@gmail.com>
2013-02-21 14:30:14 -0500

    Fig 1

--- plot/plot.tables.R
+++ plot/plot.tables.R
@@ -11,15 +11,43 @@ plot.above <- function(csv, name, width=8, height=8){
 	bolds <- unique(dt$boldmeta)
 	for(bold in bolds){
 		dtf <- dt[dt$boldmeta == bold, ]
-		p <- qplot(x=cond, y=area, data=dtf, geom="bar", stat="identity") + 	
-			theme_bw() + 
-			theme(axis.text.x=element_text(angle=-90,vjust=0.5)) +
+		p <- qplot(x=cond, y=area, fill=cond, 
+			data=dtf, geom="bar", stat="identity") + 	
+			theme_bw() +
+			ylim(0, 1) +
+			theme(axis.text.x=element_blank()) +
+			geom_hline(yintercept=0.05, color="red") +
 			theme(strip.text.x = element_text(angle=-90)) +
 			facet_grid(.~dmmeta, scales="free_x") +
 			ggtitle(paste("Bold: ", bold, sep=""))
 
 		print(p)  ## Add a page (of p) to the pdf() device
 	}
+	dev.off()
+}
+
+
+plot.above.combined <- function(csv, name, width=8, height=8){
+
+	dt <- read.table(csv, sep=",", header=TRUE)
+	
+	pdf(file=paste(name, ".pdf",sep=""),
+			width=width, height=height)  
+			## Print to a pdf device...
+
+	p <- qplot(x=cond, y=area, fill=cond, 
+		data=dt, geom="bar", stat="identity") + 	
+		facet_grid(dmmeta~boldmeta) +
+		theme_bw() +
+		ylim(0, 1) +
+		ylab("Normalized area above criterion") +
+		theme(axis.ticks.x=element_blank(), 
+				axis.text.x=element_blank(),
+				axis.title.x=element_blank(), 
+				strip.text.y = element_text(angle=0)) +
+		geom_hline(yintercept=0.05, color="red")
+
+	print(p)  ## Add a page (of p) to the pdf() device
 	dev.off()
 }
 
@@ -36,7 +64,8 @@ plot.compare <- function(csv, name, marks, ymin=-6, ymax=8, width=8, height=8){
 		dtf <- dt[dt$boldmeta == bold, ]
 		limits <- aes(ymax = mean + se, ymin=mean - se)
 		p <- ggplot(dtf, aes(x=dataset, y=mean, fill=dataset)) + 
-			geom_bar() + 			theme_bw() + 
+			geom_bar() +
+			theme_bw() + 
 			geom_hline(yintercept=marks, color="red") +
 			theme(axis.text.x=theme_blank()) +
 			theme(strip.text.y = element_text(angle=0)) +
